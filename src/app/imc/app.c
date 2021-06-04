e48e340438f04d7a74d4a238930a62a07f3359e0
Michael <chelmto3000@gmail.com>
2021-06-04 00:55:27 -0300

    New stacks

--- src/app/imc/app.c
+++ src/app/imc/app.c
@@ -1,16 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <gtk/gtk.h>
 
 // For compile
 // gcc -o app "%f" `pkg-config --libs --cflags gtk+-3.0`
 
-GtkLabel *label;
-GtkEntry *entry;
+GtkLabel *result;
+GtkEntry *inp_heigth;
+GtkEntry *inp_weigth;
+
 
 // Gtk.Button | Signal: Clicked
 void on_bt_apply_clicked(GtkWidget *widget, gpointer data)
 {
-    const gchar *text = gtk_entry_get_text(entry);
-    gtk_label_set_text(label, text);
+    const gchar *weigth_str = gtk_entry_get_text(inp_weigth);
+    const gchar *heigth_str = gtk_entry_get_text(inp_heigth);
+    
+    float heigth_float = atof(heigth_str);
+    float weigth_float = atof(weigth_str);
+    
+    float imc_float = weigth_float / (heigth_float * heigth_float);
+    
+    char text[] = "Your IMC is: ";
+    char _result[20];
+    g_snprintf(_result, 20, "%s%f.2", text, imc_float);
+    gtk_label_set_text(result, _result);
+    
 }
 
 // Gtk.Window | Signal: Destroy
@@ -38,8 +53,9 @@ int main(int argc, char *argv[])
     gtk_builder_connect_signals(builder, NULL);
     
     // Create instance for acess widget in user interface
-    label = GTK_LABEL(gtk_builder_get_object(builder, "lb_text"));
-    entry = GTK_ENTRY(gtk_builder_get_object(builder, "inp_text"));
+    result = GTK_LABEL(gtk_builder_get_object(builder, "lb_result"));
+    inp_heigth = GTK_ENTRY(gtk_builder_get_object(builder, "inp_heigth"));
+    inp_weigth = GTK_ENTRY(gtk_builder_get_object(builder, "inp_weigth"));
     GtkWidget *window = GTK_WIDGET(
         gtk_builder_get_object(builder, "main_window")
     );
